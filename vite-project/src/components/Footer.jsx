f3bf85b35f043d3a3708684f184958d4f06cef1a
maria magdalena <neumariamagdalena@gmail.com>
2024-06-25 16:25:40 +0200

    add basic structure and components

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer id="footer">this is our footer</footer>;
+}
+
+export default Footer;
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <nav>This is our Navbar</nav>;
+}
+
+export default Navbar;
