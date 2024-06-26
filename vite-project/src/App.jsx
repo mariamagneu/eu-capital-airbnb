8ca69837ea74e2785d57b633346e1de99116b951
maria magdalena <neumariamagdalena@gmail.com>
2024-06-26 15:47:47 +0200

    new components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,13 @@
 // App.jsx
 
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
-import Sidebar from './components/Sidebar.jsx';
-import Home from './components/Home.jsx';
-import About from './components/About.jsx';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
 
 function App() {
   return (
