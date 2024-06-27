0cf01b1590c51ed05352f5c65146d9bf2daee5af
Hanna Komornitzyk <hanna.komornitzyk@gmail.com>
2024-06-27 11:15:34 +0200

    ApartmentCard adjusted

--- vite-project/src/components/ApartmentCard.jsx
+++ vite-project/src/components/ApartmentCard.jsx
@@ -3,8 +3,23 @@ import React from 'react';
 function ApartmentCard({ apartment, onDelete }) {
   const isHighlyRated = apartment.review_scores_rating > 80;
 
+  const cardStyle = {
+    border: '1px solid #c7afe9',
+    backgroundColor: '#e4d4fa',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    borderRadius: '8px',
+    padding: '16px',
+    marginBottom: '16px',
+    width: '80%',
+  };
+
+  const linkStyle = {
+    fontWeight: 'bold',
+    textDecoration: 'none',
+  };
+
   return (
-    <div className="apartment-card">
+    <div className="apartment-card" style={cardStyle}>
       <h3>{apartment.name}</h3>
       <p>Location: {apartment.city}, {apartment.country}</p>
       <p>Price: ${apartment.price} per night</p>
@@ -14,7 +29,7 @@ function ApartmentCard({ apartment, onDelete }) {
       <p>Hosted by: {apartment.host_name}</p>
       <p>Accommodates: {apartment.accommodates} guests</p>
       <div>
-        <a href={apartment.picture_url.url} target="_blank" rel="noopener noreferrer">View Apartment Details</a>
+        <a href={apartment.picture_url.url} target="_blank" rel="noopener noreferrer" style={linkStyle}>View Apartment Details</a>
       </div>
       <div style={{ marginTop: '1em' }}>
         <button onClick={() => onDelete(apartment.id)}>Delete</button>
