35f83862e51e71a8b4fe9fb1967cba1f81a40574
maria magdalena <neumariamagdalena@gmail.com>
2024-06-27 20:23:54 +0200

    remove dashboard component from site, it is still in codebase though

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,7 +6,6 @@ import Footer from "./components/Footer.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
-import Dashboard from "./pages/Dashboard.jsx"; // Updated import
 import ItemDetails from "./components/ItemDetails.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import AptDetailsPage from "./pages/AptDetailsPage";
@@ -21,7 +20,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/dashboard" element={<Dashboard />} />{" "}
             {/* Updated route */}
             <Route path="/dashboard/:id" element={<ItemDetails />} />
             <Route path="*" element={<NotFound />} />
--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div id="sidebar">
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li> {/* Updated link */}
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
       </ul>
     </div>
   );
